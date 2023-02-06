cb4756a131b5df49256fb9c4a6677186673271da
AkashDY <sky.deep.1997@gail.com>
2023-02-06 16:34:08 +0530

    adding dom file

---
+++ dom.js
@@ -0,0 +1,33 @@
+// Examine the document object
+
+// console.dir(document);
+// console.log(document.domain);
+// console.log(document.URL);
+// console.log(document.title);
+
+// document.title = 'ITEMLISTS';
+
+// console.log(document.head);
+// console.log(document.body);
+// console.log(document.all);
+
+// console.log(document.forms);
+// console.log(document.links);
+
+let headerTitle = document.getElementById('header-title');
+let header = document.getElementById('main-header');
+// console.log(headerTitle);
+
+
+// headerTitle.textContent = "None";
+// headerTitle.innerText = 'Hello';
+// headerTitle.innerHTML = '<h3>Hi</h3>';
+
+header.style.borderBottom = 'solid 3px #000';
+
+const titlee = document.querySelector('.titlee');
+
+titlee.style.color = ('green');
+titlee.style.fontWeight = 'bold';
+
+
