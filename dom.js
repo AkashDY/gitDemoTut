201aa4c60a50c610828e7cff930c0d6d0d24e595
AkashDY <sky.deep.1997@gail.com>
2023-02-07 14:46:37 +0530

    new changes

--- dom.js
+++ dom.js
@@ -14,20 +14,35 @@
 // console.log(document.forms);
 // console.log(document.links);
 
-let headerTitle = document.getElementById('header-title');
-let header = document.getElementById('main-header');
+// let headerTitle = document.getElementById('header-title');
+// let header = document.getElementById('main-header');
 // console.log(headerTitle);
 
 
 // headerTitle.textContent = "None";
-// headerTitle.innerText = 'Hello';
+// // headerTitle.innerText = 'Hello';
 // headerTitle.innerHTML = '<h3>Hi</h3>';
+// console.log(headerTitle);
 
-header.style.borderBottom = 'solid 3px #000';
+// header.style.borderBottom = 'solid 3px #000';
 
-const titlee = document.querySelector('.titlee');
+// const titlee = document.querySelector('.titlee');
 
-titlee.style.color = ('green');
-titlee.style.fontWeight = 'bold';
+// titlee.style.color = ('green');
+// titlee.style.fontWeight = 'bold';
 
+// GETELEMENTBYCLASSNAME //
+
+let items = document.getElementsByClassName('list-group-item');
+console.log(items);
+console.log(items[1]);
+// items[1].style.fontWeight = 'bold';
+items[2].style.backgroundColor = 'lightgreen';
+
+// To change the HTML of list elements, we use loops //
+
+for(let i = 0; i< items.length; i++){
+    items[i].style.fontWeight = 'bold';
+    items[i].style.color = 'blue';
+}
 
