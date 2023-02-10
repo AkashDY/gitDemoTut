16a15d3bb6507e9499a8e1cfa3799c5774130d18
AkashDY <sky.deep.1997@gail.com>
2023-02-10 16:56:06 +0530

    added changes

--- JsDom/dom.js
+++ JsDom/dom.js
@@ -1,9 +1,14 @@
 let form = document.getElementById('addForm');
 var itemList = document.getElementById('items');
+var filter = document.getElementById('filter');
 
 // Form submit event /////////
 
 form.addEventListener('submit', addItem);
+
+// Filter event
+
+filter.addEventListener('keyup', filterItems);
 
 // Add item
 
@@ -12,13 +17,18 @@ function addItem(e){
 
     // get input value
     let newItem = document.getElementById('item').value;
+    let disItem = document.getElementById('discription').value;
 
     // create new li element
     let li = document.createElement('li');
+
     // add class
     li.className = 'list-group-item';
+
     // add text node with the input value
     li.appendChild(document.createTextNode(newItem));
+    li.append(document.createTextNode(disItem));
+
 
     // create del button element
     let deleteBtn = document.createElement('button');
@@ -31,8 +41,17 @@ function addItem(e){
     // append button to li
     li.appendChild(deleteBtn);
 
+    // edit button
+
+    let editBtn = document.createElement('button');
+    editBtn.className = 'btn btn-danger btn-sm float-right delete';
+    editBtn.appendChild(document.createTextNode('Edit'));
+    li.append(editBtn);
+    // itemList.appendChild(li);
+
     // append li to list
     itemList.appendChild(li);
+
 }
 
 // Delete event ///////////////
@@ -49,13 +68,25 @@ function removeItem(e){
     }
 }
 
-// ------------------------------------------------------------------------
 
-// Add edit button next to delete button
+// Filter items
 
-li = document.getElementsByClassName('list-group-item');
-let editBtn = document.createElement('button');
-editBtn.className = 'btn btn-danger btn-sm float-right delete';
-editBtn.appendChild(document.createTextNode('Edit'));
-li.appendChild(editBtn);
-itemList.appendChild(li);
+function filterItems(e){
+    // convert text to lowercase
+    let text = e.target.value.toLowerCase();
+
+    // get list
+    let items = itemList.getElementsByTagName('li');
+
+    // convert to an array
+    Array.from(items).forEach(function(item){
+        let itemName = item.firstChild.textContent;
+        let disName = item.childNodes[1].textContent;
+
+        if(itemName.toLowerCase().indexOf(text) != -1 || disName.toLowerCase().indexOf(text) != -1){
+            item.style.display = 'block';
+        } else{
+            item.style.display = 'none';
+        }
+    });
+}
